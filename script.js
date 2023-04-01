6b10fcb3d648450da92c1f3e284a8093fae1900d
ShemeerCoder-01 <118423467+ShemeerCoder-01@users.noreply.github.com>
2023-04-01 21:06:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your code here
+let input = document.querySelector("#evaluatedText");
+
+input.addEventListener('input',countWord);
+
+
+function countWord(){
+	var words = document.querySelector("#evaluatedText").value;
+
+	var splitted = words.split(' ');
+	
+	var count = 0;
+
+	for(var word in splitted){
+		if(word != ""){
+			count++;
+		}
+	}
+
+	document.querySelector("#wordCount").innerHTML = count;
+	
+}
