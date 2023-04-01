f019937856d1a6165f8a0168c1de5341c876826c
ShemeerCoder-01 <118423467+ShemeerCoder-01@users.noreply.github.com>
2023-04-01 22:08:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
-//your code here
 let input = document.querySelector("#evaluatedText");
 
-input.addEventListener('input',countWord);
+// input.addEventListener('input',countWord);
+input.addEventListener("keyup", countWord);
 
+function countWord() {
+var words = document.querySelector("#evaluatedText").value.trim();
+console.log(words);
+var splitted = words.split(" ");
+var count = 0;
 
-function countWord(){
-	var words = document.querySelector("#evaluatedText").value;
+for (var word of splitted) {
+if (word.trim() !== "") {
+count++;
+}
+}
 
-	var splitted = words.split(' ');
-	
-	var count = 0;
-
-	for(var word in splitted){
-		if(word != ""){
-			count++;
-		}
-	}
-
-	document.querySelector("#wordCount").innerHTML = count;
-	
+document.querySelector("#wordCount").innerHTML = count;
 }
